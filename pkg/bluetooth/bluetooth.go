fb1642fcbf80487508ffd9b833a8abf6663ca1c2
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2024-11-28 22:00:33 +0100

    chore: report non working instantaneous power

--- pkg/bluetooth/bluetooth.go
+++ pkg/bluetooth/bluetooth.go
@@ -10,6 +10,10 @@ var adapter = bluetooth.DefaultAdapter
 var zwiftHubUUID = "c96fb5f7-b4d5-262e-7baf-0a479225f3ab"
 var ftmsUUID = "00001826-0000-1000-8000-00805f9b34fb"
 
+// notice that the only thing that's different from the ftmsUUID is the first segment.
+// this is the case wit hall uuids
+var instantaneousPower = "00002ad2-0000-1000-8000-00805f9b34fb"
+
 func Scan() { // Enable BLE interface.
 	var zwiftHub *bluetooth.ScanResult
 	err := adapter.Enable()
@@ -61,5 +65,30 @@ func Scan() { // Enable BLE interface.
 		panic("not ftmsReady")
 	}
 
+	service := dservices[0]
+
 	fmt.Println("FTMS ready")
+
+	serviceUuid, err := bluetooth.ParseUUID(instantaneousPower)
+	if err != nil {
+		panic("could not get service UUID")
+	}
+
+	chars, err := service.DiscoverCharacteristics([]bluetooth.UUID{serviceUuid})
+	if err != nil {
+		panic("Could not get characteristics")
+	}
+
+	charsOk := len(chars) == 1
+	if !charsOk {
+		panic("Device does not have instantaneous power characteristic")
+	}
+
+	char := chars[0]
+	char.EnableNotifications(func(buf []byte) {
+		println("data:", uint8(buf[1]))
+	})
+
+	// block
+	select {}
 }
