6dfc83de5d4bb9663884450c654ad2068055619b
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2024-11-28 21:40:25 +0100

    chore: connect to device

--- pkg/bluetooth/bluetooth.go
+++ pkg/bluetooth/bluetooth.go
@@ -8,20 +8,22 @@ import (
 
 var adapter = bluetooth.DefaultAdapter
 var zwiftHubUUID = "c96fb5f7-b4d5-262e-7baf-0a479225f3ab"
+var ftmsUUID = "00001826-0000-1000-8000-00805f9b34fb"
 
 func Scan() { // Enable BLE interface.
+	var zwiftHub *bluetooth.ScanResult
 	err := adapter.Enable()
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("Finding trainer...")
-	connectChan := make(chan bool)
+	done := make(chan bool)
 	go func() {
 		err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
 			if device.Address.String() == zwiftHubUUID {
-				println("found device:", device.Address.String(), device.RSSI, device.LocalName())
-				connectChan <- true
+				zwiftHub = &device
+				done <- true
 			}
 		})
 
@@ -30,10 +32,34 @@ func Scan() { // Enable BLE interface.
 		}
 	}()
 
-	<-connectChan
+	<-done
 	err = adapter.StopScan()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("found trainer")
+
+	device, err := adapter.Connect(zwiftHub.Address, bluetooth.ConnectionParams{})
+	if err != nil {
+		panic("Cannot connect to device")
+	}
+
+	fmt.Println("Connected")
+
+	ftms, err := bluetooth.ParseUUID(ftmsUUID)
+	if err != nil {
+		panic(err)
+	}
+
+	dservices, err := device.DiscoverServices([]bluetooth.UUID{ftms})
+	if err != nil {
+		panic("not ftmsReady")
+	}
+
+	ftmsOk := len(dservices) == 1
+	if !ftmsOk {
+		panic("not ftmsReady")
+	}
+
+	fmt.Println("FTMS ready")
 }
