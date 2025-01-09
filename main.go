63bdad0ac1aaacfc749a6ea50ede89fffeb02b2a
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2025-01-09 10:54:45 +0100

    chore: remove the builder code from main.go

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log/slog"
 	"overlay/game"
 	"overlay/internal/route"
 	"overlay/internal/training"
 	"overlay/pkg/bluetooth"
 	"overlay/pkg/gpx"
-	"sync"
 )
 
 var mock = flag.Bool("m", false, "Sets up a mock trainer instead of connecting to a real trainer")
@@ -34,77 +31,17 @@ func main() {
 	training := training.NewRandom()
 	helloWorldRoute := route.New()
 
-	filePowerChan := make(chan int)
-	hasPower := trainer.Power.AddListener(filePowerChan)
+	title := "Hello World Ride"
+	gpxFile := gpx.New(title)
 
-	fileCadenceChar := make(chan int)
-	hasCadence := trainer.Cadence.AddListener(fileCadenceChar)
+	// listen for data of the trainer
+	trainer.Listen()
 
-	fileSpeedChar := make(chan int)
-	hasSpeed := trainer.Speed.AddListener(fileSpeedChar)
-
-	gpxFile := gpx.New("Hello World Ride")
+	// use the data to build a gpx file
 	go func() {
-		for {
-			wg := sync.WaitGroup{}
-			var pow int
-			var cad int
-			var speed int
-
-			wg.Add(1)
-			go func() {
-				if !hasPower {
-					wg.Done()
-				}
-
-				p := <-filePowerChan
-				pow = p
-				wg.Done()
-			}()
-
-			wg.Add(1)
-			go func() {
-				if !hasCadence {
-					wg.Done()
-				}
-
-				c := <-fileCadenceChar
-				cad = c
-				wg.Done()
-			}()
-
-			wg.Add(1)
-			go func() {
-				if !hasSpeed {
-					wg.Done()
-				}
-
-				// calculate distance in route
-				s := <-fileSpeedChar
-				speed = s
-				wg.Done()
-			}()
-
-			wg.Wait()
-
-			slog.Info(
-				fmt.Sprintf(
-					"Adding trackpoint with power %d, cadence %d and speed %d",
-					pow,
-					cad,
-					speed,
-				),
-			)
-
-			gpxFile.AddTrackpoint(gpx.NewTrackpoint(
-				"23.3581890",
-				"54.9870280",
-				gpx.WithPower(pow),
-				gpx.WithCadence(cad),
-			))
-		}
+		gpxFile.Build(trainer)
 	}()
 
-	trainer.Listen()
+	// use the data to run the game
 	game.Run(training, trainer, helloWorldRoute)
 }
