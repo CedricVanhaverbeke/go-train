9743c54ae229ca824d01b6f007790a8ce1f9a088
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2025-01-15 12:02:13 +0100

    chore: calculate speed from power

--- pkg/bluetooth/mock.go
+++ pkg/bluetooth/mock.go
@@ -11,9 +11,6 @@ import (
 type mockPowerChar struct {
 	listeners
 }
-type speedPowerChar struct {
-	listeners
-}
 
 // mock characteristic
 // that is not implemented
@@ -51,26 +48,9 @@ func (p *mockPowerChar) Write(power int) (int, error) {
 	return 0, nil
 }
 
-func (p *speedPowerChar) ContinuousRead() error {
-	go func() {
-		for {
-			p.listeners.WriteValue(rand.Intn(4) + 28)
-			time.Sleep(2 * time.Second)
-		}
-	}()
-
-	return nil
-}
-
-func (p *speedPowerChar) Write(speed int) (int, error) {
-	slog.Info("Should write " + strconv.Itoa(speed) + " to trainer")
-	return 0, nil
-}
-
 func NewMockDevice() Device {
 	return NewDevice(
 		WithPower(&mockPowerChar{}),
-		WithSpeed(&speedPowerChar{}),
 		WithCadence(&errorCadenceChar{}),
 	)
 }
